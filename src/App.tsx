fbdf5e1a70d00a953e2b527b141f88da228f60f5
mohamedhisham09 <hisham.h@ampcome.com>
2023-07-04 18:40:42 +0530

    Initial Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
-import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-// APP SCSS
+// CSS & SCSS
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
+// Pages
+import Home from "pages/Home";
+
 function App() {
-  return <div>Test</div>;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        {/* <AuthRoute path="/login" component={Login} />
+            <ProtectedRoute path="/profile/:userId" component={Profile} /> */}
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/redux/userSlice.ts
@@ -0,0 +1,47 @@
+import { createSlice } from "@reduxjs/toolkit";
+import jwtDecode from "jwt-decode";
+
+interface State {
+  user_detail: any;
+  token: string | null;
+}
+
+const initialState: State = {
+  user_detail: null,
+  token: null,
+};
+
+if (localStorage.getItem("h-shop-token")) {
+  const decodedToken: any = jwtDecode(
+    localStorage.getItem("h-shop-token") || ""
+  );
+
+  if (decodedToken.exp * 1000 < Date.now()) {
+    localStorage.removeItem("h-shop-token");
+  } else {
+    initialState.user_detail = decodedToken;
+    initialState.token = localStorage.getItem("h-shop-token");
+  }
+}
+
+const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    setUserDetail: (state, action) => {
+      state.user_detail = action.payload;
+    },
+    setToken: (state, action) => {
+      state.token = action.payload;
+    },
+    setLogout: (state) => {
+      localStorage.removeItem("h-tweet-token");
+      state.user_detail = null;
+      state.token = null;
+    },
+  },
+});
+
+export const { setUserDetail, setToken, setLogout } = userSlice.actions;
+
+export default userSlice.reducer;
